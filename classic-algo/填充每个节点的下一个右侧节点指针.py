9d98b9c16b6a09adda21da5e5768b57f4bde8285
chendong <chendong78@huawei.com>
2020-10-15 09:51:23 +0800

    2020-10-15

---
+++ classic-algo/填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,50 @@
+"""
+给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self,
+                 val: int = 0,
+                 left: 'Node' = None,
+                 right: 'Node' = None,
+                 next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        stack = [(root, 0)]
+        while True:
+            node, lv = stack.pop(0)
+            if node.left:
+                stack.append((node.left, lv + 1))
+            if node.right:
+                stack.append((node.right, lv + 1))
+            if not stack:
+                break
+            if lv == stack[0][1]:
+                node.next = stack[0][0]
+        return root
