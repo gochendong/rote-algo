beeb72dd193bc90aeb452ee4b32c80a923a8b000
chendong <chendong78@huawei.com>
2020-12-02 10:16:55 +0800

    2020-12-02

---
+++ classic-algo/动态规划(复杂贪心)/礼物的最大价值.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        for i in range(1, m):
+            grid[i][0] += grid[i - 1][0]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j - 1]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += max(grid[i - 1][j], grid[i][j - 1])
+        return grid[-1][-1]
